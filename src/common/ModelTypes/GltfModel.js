f785f7dd14d52516bb60730f2ade036f95cf6f49
Thomas Guillouët <thomas.guillouet@outlook.com>
2020-01-15 15:54:51 +0100

    Ajout du support des modeles 3d

---
+++ src/common/ModelTypes/GltfModel.js
@@ -0,0 +1,29 @@
+import Model from "./Model";
+
+export default class GltfModel extends Model {
+    constructor(modelInfos) {
+        super(modelInfos)
+    }
+
+    createAsset() {
+        const { name: modelName, type } = this.modelInfos;
+
+        const asset = document.createElement("a-assets");
+    
+        const modelAssetItem = document.createElement("a-asset-item");
+        modelAssetItem.id = `${modelName}-model`;
+        modelAssetItem.setAttribute("src", `/${modelName}.${type}`);
+        asset.appendChild(modelAssetItem);
+    
+        return asset;
+    }
+    
+    createEntity() {
+        const { name: modelName } = this.modelInfos;
+        const entity = document.createElement("a-entity");
+    
+        entity.setAttribute("gltf-model", `##${modelName}-model`);
+
+        return entity
+    }
+}
---
+++ src/common/ModelTypes/Model.js
@@ -0,0 +1,36 @@
+export default class Model {
+    constructor(modelInfos) {
+        if (this.constructor === Model) {
+            throw new TypeError("You cannot use the class Model directly")
+        }
+        this.modelInfos = modelInfos
+    }
+
+    createAsset() {
+        throw new Error("The function createAsset is not implemented")
+    }
+
+    createEntity() {
+        throw new Error("The function createEntity is not implemented")
+    }
+
+    createInFrame(frame) {
+        frame.appendChild(this.createAsset());
+
+        let entity = this.createEntity();
+        if (this.modelInfos.parameters) entity = this.applyEntityParameters(entity)
+
+        frame.appendChild(entity);
+    }
+
+    applyEntityParameters(entity) {
+        const { parameters } = this.modelInfos;
+
+        Object.keys(parameters).forEach(key => {
+            let value = parameters[key]
+            entity.setAttribute(key, value);
+        });
+        
+        return entity
+    }
+}
