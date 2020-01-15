f11de937a605d952c76b2962f36ff70c4764e6f8
Thomas Guillouët <thomas.guillouet@outlook.com>
2020-01-15 15:55:24 +0100

    Gestion des parametres de l'urls

--- src/common/uri.js
+++ src/common/uri.js
@@ -7,9 +7,15 @@ const QUERY_PARAM_KEYVAL_SEPARATOR = "="
 
 /**
  * Get the params contained in the url
+ * @returns An object who contains all query parameters
  */
 export function getUrlParams() {
     const queryString = window.location.href.split(QUERY_START_CHAR)[1]
+
+    if (!queryString) {
+        return {}
+    }
+
     return queryString
         .split(QUERY_PARAM_SEPARATOR)
         .map((uriElement) => {
