f782270f6aca2a7de3ced22413d875ad9b7c9a61
Thomas Guillouët <thomas.guillouet@outlook.com>
2020-01-16 09:24:47 +0100

    Gestion d'erreur

--- src/common/api/AssetApi.js
+++ src/common/api/AssetApi.js
@@ -2,6 +2,14 @@ import { firebase } from "@firebase/app"
 import "@firebase/firestore"
 
 export function getAsset(projectName, assetId) {
+    if (!projectName) {
+        throw new Error("The project name need to be filled")
+    }
+
+    if (!assetId) {
+        throw new Error("The asset id need to be filled")
+    }
+
     let ref = firebase.firestore().collection(`projects`).doc(projectName)
     return  ref.collection("assets").doc(assetId).get().then((asset) => {
         let assetData = asset.data()
