f665e35d388c76f0c1031892af70cd8d370c9d4a
Thomas Guillouët <thomas.guillouet@outlook.com>
2020-03-24 12:10:55 +0100

    Testing the html entity generation

--- src/tests/Model.test.js
+++ src/tests/Model.test.js
@@ -27,3 +27,53 @@ describe('3d Models instanciations', function() {
         expect(model).toEqual(expect.any(GltfModel));
     })
 });
+
+describe('HTML elements generation', function () {
+    it('Test if the created obj model has the right attributes without material and parameters', function() {
+        const model = new ModelFactory().makeModel({
+            name: "test",
+            type: "obj",
+            model: "url-to-the-model"
+        });
+
+        const expectedElement = document.createElement("a-obj-model");
+        expectedElement.setAttribute("src", "#test-model");
+
+        expect(model.createEntity().outerHTML).toBe(expectedElement.outerHTML);
+    });
+
+    it('Test if the created obj model has the right attributes with material and without parameters', function() {
+        const model = new ModelFactory().makeModel({
+            name: "test",
+            type: "obj",
+            model: "url-to-the-model",
+            material: "url-to-the-material"
+        });
+        const expectedElement = document.createElement("a-obj-model");
+        expectedElement.setAttribute("src", "#test-model");
+        expectedElement.setAttribute("mtl", "#test-material");
+
+        expect(model.createEntity().outerHTML).toBe(expectedElement.outerHTML);
+    });
+
+    it('Test if the created obj model has the right attributes with parameters', function() {
+        const model = new ModelFactory().makeModel({
+            name: "test",
+            type: "obj",
+            model: "url-to-the-model",
+            material: "url-to-the-material",
+            parameters: {
+                scale: 'xVal yVal zVal'
+            }
+        });
+        let entity = model.createEntity();
+        entity = model.applyEntityParameters(entity);
+
+        const expectedElement = document.createElement("a-obj-model");
+        expectedElement.setAttribute("src", "#test-model");
+        expectedElement.setAttribute("mtl", "#test-material");
+        expectedElement.setAttribute("scale", "xVal yVal zVal");
+
+        expect(entity.outerHTML).toBe(expectedElement.outerHTML);
+    });
+})
