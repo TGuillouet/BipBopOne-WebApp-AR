f583a289f74983ee9766e61bfd8f76eeee82ee55
Thomas Guillouët <thomas.guillouet@outlook.com>
2020-03-24 12:10:34 +0100

    Formatting

--- src/tests/Model.test.js
+++ src/tests/Model.test.js
@@ -6,7 +6,7 @@ import GltfModel from "../ModelTypes/GltfModel";
 describe('3d Models instanciations', function() {
 	it('Test if a raw model can instanciated', function() {
         const instanciateRawModel = () => {
-            const model = new Model()
+            const model = new Model();
         };
 		expect(instanciateRawModel).toThrow(TypeError);
     });
@@ -16,7 +16,7 @@ describe('3d Models instanciations', function() {
             name: "test",
             type: "obj"
         });
-        expect(model).toEqual(expect.any(ObjModel))
+        expect(model).toEqual(expect.any(ObjModel));
     })
 
     it('Test if we can instanciate a .gltf 3d model', function() {
@@ -24,6 +24,6 @@ describe('3d Models instanciations', function() {
             name: "test",
             type: "gltf"
         });
-        expect(model).toEqual(expect.any(GltfModel))
+        expect(model).toEqual(expect.any(GltfModel));
     })
 });
